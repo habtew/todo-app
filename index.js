075d7e9bef25e48eb043cdbd49653d1060d92166
Habtamu <habtamuwolde92@gmail.com>
2023-03-27 22:01:15 +0530

    added more actions like to select and view only active todos , to select and view completed list , select and view all todos delete todos

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const img = document.querySelector('.img')
 let data = JSON.parse(localStorage.getItem('todos')) || []
 console.log(data)
 document.body.addEventListener('click', (e)=>{
-    // console.log()
     if(e.target.matches('.btn')){
         handleDelete(e)
     }
@@ -13,6 +12,15 @@ document.body.addEventListener('click', (e)=>{
     }
     if(e.target.matches('.fav-icon')){
         handleFavIcon(e)
+    }
+    if(e.target.matches('.active-todo')){
+        handleActiveTodo(e)
+    }
+    if(e.target.matches('.complete-todo')){
+        handleCompletedTodo(e)
+    }
+    if(e.target.matches('.all-todo')){
+        renderHtml(data)
     }
 })
 
@@ -29,7 +37,7 @@ function handleInput(e){
         if(e.key == 'Enter' && e.target.value !== ''){
             const inpu = {
                 todo: e.target.value,
-                id: data.length + 1,
+                id: e.target.value.length + data.length + 1,
                 isComplete: false
             }
             data.unshift(inpu)
@@ -49,17 +57,28 @@ function handleFavIcon(e){
     })
     let updatedData = JSON.stringify(data)
     localStorage.setItem('todos', updatedData)
-    console.log(data)
+    renderHtml(data)
 }
+
+function handleActiveTodo(e){
+    const target = data.filter(item => !item.isComplete)
+    renderHtml(target)
+}
+
+function handleCompletedTodo(e){
+    const target = data.filter(item => item.isComplete)
+    renderHtml(target)
+}
+
 
 function renderHtml(data){
     let html = ''
     html += data.map(item => {
         return `
         <div class="todo-items" >
-            <div class="fav-icon fav-icon-toggle" id=${item.id}>       
+            <div class="fav-icon ${item.isComplete?'fav-icon-toggle':''}" id=${item.id}>
             </div>
-            <p id=${item.id}>${item.todo}</p>
+            <p id=${item.id} class="${item.isComplete && 'strike'}">${item.todo}</p>
             <button >
                 <img src="./images/icon-cross.svg" id=${item.id} class="btn"/>
             </button>
